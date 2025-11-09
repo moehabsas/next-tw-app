5b4d4efe2ef0b46334aed7dbacc15a2b236a6d51
Mohamed El-Gedawy <moehabsas@gmail.com>
2025-11-09 16:25:41 +0200

    chore(tailwind): setup tailwind postcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable}`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
