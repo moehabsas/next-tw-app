4f50361e713b93ab9625f1500f1ef6de0ebdcbc6
Mohamed El-Gedawy <moehabsas@gmail.com>
2025-11-21 14:30:41 +0200

    style(theme): configure theme tokens and base body styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
